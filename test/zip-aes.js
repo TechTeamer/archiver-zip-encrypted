bb56e29a9671a4beb7e180963606b4e84b2268dd
horvathbalazshbal <horvathbalazshbal@gmail.com>
2026-05-20 09:25:03 +0200

    chore: bump archiver to v8 and update tests and zip-encrypted.js

--- test/zip-aes.js
+++ test/zip-aes.js
@@ -1,4 +1,5 @@
-const archiver = require('archiver');
+const { ZipArchive } = require('archiver');
+const zipArchive = (format, options) => new ZipArchive(format, options);
 const fs = require('fs');
 const cp = require('child_process');
 const rmrf = require('rimraf');
@@ -24,7 +25,7 @@ describe('zip-aes', () => {
     });
 
     it('should pack with zip-aes and unpack with 7z', (done) => {
-        let archive = archiver.create('zip-encrypted', {zlib: {level: 8}, encryptionMethod: 'aes256', password: '123'});
+        let archive = zipArchive('zip-encrypted', {zlib: {level: 8}, encryptionMethod: 'aes256', password: '123'});
         archive.append(fs.createReadStream('./test/resources/test.txt'), { // with stream
             name: 'test.txt'
         });
@@ -66,7 +67,7 @@ describe('zip-aes', () => {
     // setting compression to 0 to overcome bug (?) in 16.02 7z (only available version of 7z in travis)
     // modern 7z (and WinZip as well) on Windows 10 does unpack compressed encrypted empty files w/o issues
     it('should pack empty file with zip-aes and unpack with 7z', (done) => {
-        let archive = archiver.create('zip-encrypted', {zlib: {level: 0}, encryptionMethod: 'aes256', password: '123'});
+        let archive = zipArchive('zip-encrypted', {zlib: {level: 0}, encryptionMethod: 'aes256', password: '123'});
         archive.append(fs.createReadStream('./test/resources/empty-file.txt'), {
             name: 'test.txt'
         });
@@ -95,7 +96,7 @@ describe('zip-aes', () => {
     });
 
     it('should pack directory with zip-aes and unpack with 7z', (done) => {
-        let archive = archiver.create('zip-encrypted', {zlib: {level: 8}, encryptionMethod: 'aes256', password: '123'});
+        let archive = zipArchive('zip-encrypted', {zlib: {level: 8}, encryptionMethod: 'aes256', password: '123'});
         archive.directory('./test/resources/dir', 'dir');
         archive.finalize();
 
--- test/zip20.js
+++ test/zip20.js
@@ -4,29 +4,19 @@ const cp = require('child_process');
 const rmrf = require('rimraf');
 const should = require('should');
 const path = require('path');
-
+const ZipEncrypted = require('../lib/zip-encrypted');
 /**
  * This test assumes 7-Zip installed in the system and available in path
  */
 describe('zip-crypto', () => {
-    before(() => {
-        try {
-            archiver.registerFormat('zip-encrypted', require('../lib/zip-encrypted'));
-        } catch (e) {
-            // already registered
-        }
-    });
-
     beforeEach(() => {
         rmrf.sync('./target');
         fs.mkdirSync('./target', {recursive: true});
     });
 
     it('should pack stream zip-crypto/unpack 7z', (done) => {
-        let archive = archiver.create('zip-encrypted', {zlib: {level: 8}, encryptionMethod: 'zip20', password: Buffer.from('123')});
-        archive.append(fs.createReadStream('./test/resources/test.txt'), {
-            name: 'test.txt'
-        });
+        let archive = new ZipEncrypted({zlib: {level: 8}, encryptionMethod: 'zip20', password: Buffer.from('123')});
+        archive.append(fs.createReadStream('./test/resources/test.txt'),{ name: 'test.txt'});
         archive.finalize();
 
         let out = fs.createWriteStream('./target/test.zip');
@@ -46,10 +36,8 @@ describe('zip-crypto', () => {
     });
 
     it('should pack string zip-crypto/unpack 7z', (done) => {
-        let archive = archiver.create('zip-encrypted', {zlib: {level: 8}, encryptionMethod: 'zip20', password: Buffer.from('123')});
-        archive.append('test data', {
-            name: 'test.txt'
-        });
+        let archive = new ZipEncrypted({zlib: {level: 8}, encryptionMethod: 'zip20', password: Buffer.from('123')});
+        archive.append('test data', {name: 'test.txt'});
         archive.finalize();
 
         let out = fs.createWriteStream('./target/test.zip');
